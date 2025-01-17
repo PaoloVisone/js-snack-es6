19c4e0e0e0ad4f75162cc0950e6a77059fd26555
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-17 02:08:05 +0100

    fix p to h1 and add variables

--- script.js
+++ script.js
@@ -32,6 +32,14 @@ const bici =
 
 const message = document.getElementById('message')
 
+// Creo una variabile per il peso e una per il nome
+
+let pesoBici = bici[5].peso;
+
+let nomeBici = bici[5].nome;
+
+console.log(pesoBici);
+
 
 
 
