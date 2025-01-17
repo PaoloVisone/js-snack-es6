098ae149bd9eb4488e83205ec12e9c49ddb941ee
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-17 01:47:58 +0100

    Start project

---
+++ script.js
@@ -0,0 +1,29 @@
+// Creo un array di oggetti
+
+const bici =
+    [
+        {
+            nome: 'Cervelo',
+            peso: 3
+        },
+        {
+            nome: 'Pinarello',
+            peso: 7
+        },
+        {
+            nome: 'Colnago',
+            peso: 11
+        },
+        {
+            nome: 'BMC',
+            peso: 9
+        },
+        {
+            nome: 'Specialized',
+            peso: 15
+        },
+        {
+            nome: 'Bianchi',
+            peso: 1
+        },
+    ];
